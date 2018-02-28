e01238fca813dc690c02cc6b92a51caace0628e9
Rico <anththompson84@gmail.com>
2018-02-28 17:58:32 -0500

    Initial commit

---
+++ TrainingApp/TrainingApp/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TrainingApp
+//
+//  Created by Anthony Thompson on 2/27/18.
+//  Copyright © 2018 GSU Team 5. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var Usertxt: UITextField!
+    
+    @IBOutlet weak var passwordtxt: UITextField!
+    
+    
+    
+    @IBAction func LoginBtn(_ sender: Any) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
---
+++ TrainingApp/TrainingApp/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  TrainingApp
+//
+//  Created by Anthony Thompson on 2/27/18.
+//  Copyright © 2018 GSU Team 5. All rights reserved.
+//
+
+import Foundation
+
+import RealmSwift
+
+class User: Object{
+    @objc dynamic var User: String = ""
+    @objc dynamic var Password = ""
+}
